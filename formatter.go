6edd83b95c11bebe03d3f193bbfcf76f6c89a087
Vivian Akinyi <akinyivivian0729@gmail.com>
2026-07-01 13:31:54 +0300

    Implemented text formatting

--- formatter.go
+++ formatter.go
@@ -1,1 +1,44 @@
 package main
+
+import "strings"
+
+func ProcessUpper(text string) string {
+	words := strings.Fields(text)
+	for i := 1; i < len(words); i++ {
+		if words[i] == "(up)" {
+			words[i-1] = strings.ToUpper(words[i-1])
+			words = append(words[:i], words[i+1:]...)
+
+		}
+
+	}
+	return strings.Join(words, " ")
+
+}
+
+func ProcessLower(text string) string {
+	words := strings.Fields(text)
+	for i := 1; i < len(words); i++ {
+		if words[i] == "(low)" {
+			words[i-1] = strings.ToLower(words[i-1])
+			words = append(words[:i], words[i+1:]...)
+
+		}
+
+	}
+	return strings.Join(words, " ")
+}
+
+func ProcessCap(text string) string {
+	words := strings.Fields(text)
+	for i := 1; i < len(words); i++ {
+		if words[i] == "(cap)" {
+			word := words[i-1]
+			words[i-1] = strings.ToUpper(word[:1]) + strings.ToLower(word[1:])
+			words = append(words[:i], words[i+1:]...)
+
+		}
+
+	}
+	return strings.Join(words, " ")
+}
--- processor.go
+++ processor.go
@@ -3,5 +3,8 @@ package main
 func ProcessText(text string) string {
 	text = ProcessHex(text)
 	text = ProcessBinary(text)
+	text = ProcessUpper(text)
+	text = ProcessLower(text)
+	text = ProcessCap(text)
 	return text
 }
