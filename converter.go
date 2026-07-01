570eb24e12bdd6c0c26ac868f293c534dd5ed6ca
Vivian Akinyi <akinyivivian0729@gmail.com>
2026-07-01 10:34:56 +0300

    Implement hexadecimal and binary text conversions

--- converter.go
+++ converter.go
@@ -21,3 +21,23 @@ func ProcessHex(text string) string {
 
 	return strings.Join(words, " ")
 }
+
+func ProcessBinary(text string) string {
+	words := strings.Fields(text)
+
+	for i := 1; i < len(words); i++ {
+		if words[i] == "(bin)" {
+			binaryNumber := words[i-1]
+			number, err := strconv.ParseInt(binaryNumber, 2, 64)
+			if err == nil {
+				words[i-1] = strconv.FormatInt(number, 10)
+				words = append(words[:i], words[i+1:]...)
+
+			}
+
+		}
+
+	}
+	return strings.Join(words, " ")
+
+}
--- processor.go
+++ processor.go
@@ -2,5 +2,6 @@ package main
 
 func ProcessText(text string) string {
 	text = ProcessHex(text)
+	text = ProcessBinary(text)
 	return text
 }
